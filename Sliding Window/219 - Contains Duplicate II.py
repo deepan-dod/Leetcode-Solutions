e983987df78c051d32b8f79dd9b37c02584179e1
DEEPAN <deebandpm@gmail.com>
2026-07-03 19:17:17 +0530

    Added New Problem

---
+++ Sliding Window/219 - Contains Duplicate II.py
@@ -0,0 +1,31 @@
+'''Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false'''
+
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        d={}
+        for i in range(len(nums)):
+            if nums[i] in d and i-d[nums[i]]<=k:
+                return True
+            d[nums[i]]=i
+        return False
