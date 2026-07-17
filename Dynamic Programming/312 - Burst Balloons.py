e4102ff92faffcf164b1c179345e40dfe513f22b
DEEPAN <deebandpm@gmail.com>
2026-07-17 11:11:18 +0530

    new

---
+++ Dynamic Programming/312 - Burst Balloons.py
@@ -0,0 +1,29 @@
+'''You are given n balloons, indexed from 0 to n - 1. Each balloon is painted with a number on it represented by an array nums. You are asked to burst all the balloons.
+
+If you burst the ith balloon, you will get nums[i - 1] * nums[i] * nums[i + 1] coins. If i - 1 or i + 1 goes out of bounds of the array, then treat it as if there is a balloon with a 1 painted on it.
+
+Return the maximum coins you can collect by bursting the balloons wisely.
+
+ 
+
+Example 1:
+
+Input: nums = [3,1,5,8]
+Output: 167
+Explanation:
+nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167'''
+
+
+
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        n=len(nums)
+        dp=[[0]*(n+2) for _ in range(n+2)]
+        nums=[1]+nums+[1]
+        for i in range(n,0,-1):
+            for j in range(i,n+1):
+                for k in range(i,j+1):
+                    c=nums[i-1]*nums[k]*nums[j+1]+dp[i][k-1]+dp[k+1][j]
+                    dp[i][j]=max(dp[i][j],c)
+        return dp[1][n]
