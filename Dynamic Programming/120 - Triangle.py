c1b3bbd2006066f8a42336b1658df3638b8d61ba
DEEPAN <deebandpm@gmail.com>
2026-07-28 16:14:14 +0530

    new

---
+++ Dynamic Programming/120 - Triangle.py
@@ -0,0 +1,27 @@
+'''Given a triangle array, return the minimum path sum from top to bottom.
+
+For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+
+ 
+
+Example 1:
+
+Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+Output: 11
+Explanation: The triangle looks like:
+   2
+  3 4
+ 6 5 7
+4 1 8 3
+The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).'''
+
+
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        dp = triangle[-1][:]
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1):
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
+        return dp[0]
