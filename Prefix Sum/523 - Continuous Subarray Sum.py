c92519e38df035735b3eef09a6c96ef42f06aee9
DEEPAN <deebandpm@gmail.com>
2026-07-29 09:50:51 +0530

    NEW

---
+++ Prefix Sum/523 - Continuous Subarray Sum.py
@@ -0,0 +1,33 @@
+'''Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+
+A good subarray is a subarray where:
+
+its length is at least two, and
+the sum of the elements of the subarray is a multiple of k.
+Note that:
+
+A subarray is a contiguous part of the array.
+An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+ 
+
+Example 1:
+
+Input: nums = [23,2,4,6,7], k = 6
+Output: true
+Explanation: [2, 4] is a continuous subarray of size 2 whose elements sum up to 6.'''
+
+
+
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+        pref = {0:-1}
+        p = 0
+        for i in range(n):
+            p += nums[i]
+            p %= k
+            if p not in pref:
+                pref[p] = i
+            elif i - pref[p] >= 2:
+                return True
+        return False
